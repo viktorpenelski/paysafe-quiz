25e361af8a2f2e0d1327d8e23e8d1b92110397d6
Viktor Penelski <viktor.penelski@paysafe.com>
2019-05-27 19:38:36 +0300

    initial

---
+++ assets/quiz.js
@@ -0,0 +1,66 @@
+$(function () {
+
+    let step = 0;
+
+    $("input").checkboxradio();
+    $("fieldset").controlgroup();
+    $("button").button().click(function () {
+        var id = $(this).closest("div").attr("id");
+        if (id == "step-0") {
+            progress();
+            $("#step-0").hide();
+            $("#step-1").show();
+        } else if (id == "step-1") {
+            $("#step-1").hide();
+            $("#step-2").show();
+        } else if (id == "step-2") {
+            $("#step-2").hide();
+            $("#step-3").show();
+        } else if (id == "step-3") {
+            finish();
+        } else if (id == "step-4") {
+            window.location.reload();
+        }
+    });
+    var progressbar = $("#progressbar");
+    var progressLabel = $(".progress-label");
+
+    progressbar.progressbar({
+        max: 90,
+        value: 90,
+        change: function () {
+            progressLabel.text(progressbar.progressbar("value") + " seconds");
+        }
+    });
+
+    function progress() {
+        var val = progressbar.progressbar("value");
+        progressbar.progressbar("value", val - 1);
+        if (val > 0) {
+            setTimeout(progress, 1000);
+        } else {
+            progressLabel.text("Result");
+            finish();
+        }
+    }
+
+    function finish() {
+        progressbar.progressbar("value", 0);
+        $("#step-1").hide();
+        $("#step-2").hide();
+        $("#step-3").hide();
+        var correct = $("label[data-correct=true].ui-checkboxradio-checked").length;
+        $("#result").text(correct);
+        $("#step-4").show();
+    }
+
+    progressbar.find(".ui-progressbar-value").css("background-color", "#66cc99");
+});
+
+class Question {
+    constructor(question, answers, correctAnswerId) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswerId = correctAnswerId;
+    }
+}
